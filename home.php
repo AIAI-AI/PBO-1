4a6c251b4c46ec1b5d1ec721e0a90a6658c4eb7c
AIAI-AI <h1101241044@student.untan.ac.id>
2025-08-28 13:52:04 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+$date = date('Y-m-d H:i:s');
+?>
+<!doctype html>
+<html lang="id"> 
+  <head>
+    <meta charset="utf-8">
+    <title>Home PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style></style>
+  </head>
+  <body>
+    <h1>Selamat datang di Home</h1>
+    <p>Waktu server: <strong><?php echo htmlspecialshers($date, END-QUOTES, 'UTF-8'); ?></strong></p>
+    <p><a href='index.php'>Back to Index</a></p>
+  </body>
+</html>
